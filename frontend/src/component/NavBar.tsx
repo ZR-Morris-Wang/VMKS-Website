51700acde3a534456f64f88e20bd6d2bee81f26e
YBA857142 <grcgs2601@gmail.com>
2023-08-28 01:33:02 +0800

    EDITOR-#103 commit again

--- frontend/src/component/NavBar.tsx
+++ frontend/src/component/NavBar.tsx
@@ -33,7 +33,8 @@ export const NavBar = () => {
   const navigate = useNavigate();
   return(
     <nav>
-      <div id="header" style={{ paddingLeft:'16px', paddingRight:'16px'}} className="button-container">
+
+      <div id="header" className="button-container">
         <Link to='/'><img src={logo} alt="logo" height="50"/></Link>
         <Button variant="contained"  size="medium" style={{float:"right", backgroundColor:'#67b9c7ff'}} onClick={()=>navigate("/LoginPage")}>
           <Link to="/LoginPage" style={{ textDecoration: "none", color: "white" }}>
@@ -41,6 +42,7 @@ export const NavBar = () => {
           </Link>
         </Button>
       </div>
+
       <Box>
         <AppBar position="static" style={{backgroundColor:'#67B9C7'}}>
           <Toolbar style={{display: 'flex', justifyContent: 'flex-end', minHeight:'24px'}}>
@@ -62,41 +64,12 @@ export const NavBar = () => {
               </dl>
 
               <dl>
-                <dt><Button variant="text" size="large" style={Object.assign({borderRightWidth: '3px',},buttonstyle)} onClick={()=>navigate("/TutorialPage")}>新手教學</Button></dt>
+                <dt><Button variant="text" size="large" style={Object.assign({borderRightWidth: '3px'},buttonstyle)} onClick={()=>navigate("/TutorialPage")}>新手教學</Button></dt>
               </dl>
-            {/* <dl>
-              <dt><Link to='IntroductionPage'>MKS介紹</Link></dt>
-              <dd><Link to='IntroductionPage'>MKS簡介</Link></dd>
-              <dd>管理員班表</dd>
-              <dd>使用者規範</dd>
-            </dl> */}
-            {/* <dl>
-              <dt><Link to='TutorialPage'>新手教學</Link></dt>
-              <dd>互動式導覽</dd>
-              <dd>VMKS功能一覽</dd>
-            </dl> */}
-
-            {/* <dl>
-              <Link to='MapPage'>地圖導覽</Link>
-            </dl> */}
-            {/* <dl>
-              <dt><Link to='MaterialAndToolPage'>資源一覽</Link></dt>
-              <dd>耗材</dd>
-              <dd>材料</dd>
-              <dd>工具</dd>
-              <dd>機台</dd>
-            </dl>
-            <dl>
-              <dt><Link to='TutorialPage'>新手教學</Link></dt>
-              <dd>互動式導覽</dd>
-              <dd>VMKS功能一覽</dd>
-            </dl>  */}
+              
             </Toolbar>
         </AppBar>
       </Box>
-      
-      
-      {/* <Link to='ShoppingList'>借用清單</Link> */}
       
     </nav>
   ) 
